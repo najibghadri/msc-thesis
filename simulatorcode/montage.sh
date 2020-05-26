133685d297fa2dab0fc4bd4149fde3110d178082
najibghadri <najibg96@gmail.com>
2020-05-26 20:50:33 +0200

    ok

--- simulatorcode/montage.sh
+++ simulatorcode/montage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # run this in Rendering/{number}, black.jpg in Rendering folder
 
-mkdir montage
+mkdir montage${2-}
 for N in {0..4000}
 do
-	if [ -e out/${N}FL.jpg ]
+	if [ -e out/${N}FL${2-}.jpg ]
 	then
 		echo $N
 		montage -geometry +0+0 -tile 3x2  ${1-out}/${N}LC2${2-}.jpg ${1-out}/${N}FL${2-}.jpg ${1-out}/${N}RC1${2-}.jpg ${1-out}/${N}L2${2-}.jpg ../black.jpg ${1-out}/${N}R1${2-}.jpg montage${2-}/${N}montage.jpg
